24f2711b48bfa804bfac3e399f05a8238e2fffbb
Joseph Hooker <jcolehooker@gmail.com>
2021-09-10 21:39:12 -0500

    Completed problem 4; attempted to use a match-case (Py version of switch-case) but system does not like anything v3+

--- functions-basic2.py
+++ functions-basic2.py
@@ -1,3 +1,6 @@
+from typing import Match
+import numpy as np
+
 #1 - Countdown
 # def countdown(startNum):
 #     counted=[]
@@ -17,12 +20,49 @@
 # print(printReturn(arr2))
 
 #3 - First Plus Length
-import numpy as np
-def firstPlusLen(arr):
-    return arr[0] + len(arr)
-arrScale = (int(input("Size of array?(1-15): ")))
-ronArrTest = [None]*arrScale
-for i in range(len(ronArrTest)):
-    ronArrTest[i] = np.random.randint(-10,200)
-print(ronArrTest)
-print(firstPlusLen(ronArrTest))
+# def firstPlusLen(arr):
+#     return arr[0] + len(arr)
+# arrScale = (int(input("Size of array?(1-15): ")))
+# ronArrTest = [None]*arrScale
+# for i in range(len(ronArrTest)):
+#     ronArrTest[i] = np.random.randint(-10,200)
+# print(ronArrTest)
+# print(firstPlusLen(ronArrTest))
+
+#4 - Values Greater Than Second
+def valsGreaterThan2nd(arr):
+    x = len(arr)
+    arr2=[]
+    if x < 2:
+        return False
+    elif x == 2:
+        if arr[0]>arr[1]:
+            arr2.append(arr[0])
+    else:
+        if arr[0]>arr[1]:
+                arr2.append(arr[0])
+        for i in range(2,x):
+            if arr[i]>arr[1]:
+                arr2.append(arr[i]) 
+    # match x:
+    #     case 0:
+    #         return False
+    #     case 1:
+    #         return False
+    #     case 2:
+    #         if arr[0]>arr[1]:
+    #             arr2.append(arr[0])
+    #     case _:
+    #         if arr[0]>arr[1]:
+    #             arr2.append(arr[0])
+    #         for i in range(2,x):
+    #             if arr[i]>arr[1]:
+    #                 arr2.append(arr[i]) 
+    print(len(arr2))
+    return arr2
+arrScale = (int(input("Size of array?(0 or larger - but the fun starts at 2): ")))
+newArr = [None]*arrScale
+for i in range(len(newArr)):
+    newArr[i] = np.random.randint(-200,200)
+print(newArr)
+print(valsGreaterThan2nd(newArr))
